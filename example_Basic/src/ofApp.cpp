a292ea983b4f22f0e2e33c3b4d19b54ada7daf28
JeongHo Park <jeonghopark@jeonghopark.de>
2015-09-20 17:30:17 +0200

    update example for oF 0.9, Mac OSX El Capitan, Xcode 7.0

--- example_Basic/src/ofApp.cpp
+++ example_Basic/src/ofApp.cpp
@@ -1,7 +1,7 @@
-#include "testApp.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void ofApp::setup(){
   ofSoundStreamSetup(2, 0, this, 44100, 256, 4);
   
   // create a new oscillator which we'll use for the actual audio signal
@@ -26,60 +26,60 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void ofApp::update(){
 
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::draw(){
 
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void ofApp::keyPressed(int key){
 
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
+void ofApp::keyReleased(int key){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y ){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h){
 
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg){
 
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
 
-void testApp::audioRequested (float * output, int bufferSize, int nChannels){
+void ofApp::audioRequested (float * output, int bufferSize, int nChannels){
   synth.fillBufferOfFloats(output, bufferSize, nChannels);
 }
---
+++ example_Events/src/ofApp.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxTonic.h"
+
+using namespace Tonic;
+
+
+class FlashingRectangle{
+    
+    bool isOn;
+    ofRectangle rect;
+    ofColor color;
+    
+    ofColor createColor(float filterVal){
+        
+        float colorIntensity =0.2 + 0.8 * (filterVal);
+        return ofColor(55 * colorIntensity, 55 * colorIntensity, 255 * colorIntensity);
+    }
+    
+public:
+    
+    FlashingRectangle(float xPosition, float width):
+    rect(xPosition, 0, width, ofGetScreenHeight()){
+        color = createColor(0);
+    }
+    
+    void pulseHappened(float& val){
+        isOn = val;
+    }
+    
+    void filterFreqChanged(float & val){
+        rect.setHeight((1 - val) * ofGetScreenHeight());
+        color = createColor(val);
+    }
+    
+    void draw(){
+        if(isOn){
+            ofSetColor(color);
+            ofRect(rect);
+        }
+    }
+    
+};
+
+
+class ofApp : public ofBaseApp{
+    
+    ofxTonicSynth synth;
+    vector<FlashingRectangle*> flashingRectangles;
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    void audioRequested (float * output, int bufferSize, int nChannels);
+    
+};
